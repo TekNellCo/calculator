99e9df41422e62d6637a93e21d1ac2d8d73f81a3
NellyNell <129366078+TekNellCo@users.noreply.github.com>
2023-07-12 17:31:27 -0400

    add audio sound effect

--- script.js
+++ script.js
@@ -5,16 +5,29 @@ const screen = document.querySelector('.screen');
 const equal = document.querySelector('.equal');
 const clear = document.querySelector('.clear');
 const back = document.querySelector('.back');
+const plusminus =document.querySelector('.plusminus');
+const operator =document.querySelector('.operator');
+
+
+
+
+const audio = new Audio('/sounds/click.mp3')
+
+
+plusminus.addEventListener('click',()=>{ audio.cloneNode().play();screen.value == screen.value ? screen.value = -screen.value : screen.value; });
+
 
 numberKey.forEach(whatever => {
     whatever.addEventListener('click', (e)=>{
+        audio.cloneNode().play();
         let value = e.target.dataset.num
         screen.value += value;
-        
+     
     })
 })
 
 equal.addEventListener('click',(e)=>{
+    audio.cloneNode().play();
     if(screen.value === ''){
         screen.value = '';
     }else{
@@ -24,11 +37,14 @@ equal.addEventListener('click',(e)=>{
 });
 
 clear.addEventListener('click',()=>{
+    audio.cloneNode().play();
     screen.value= '';
 });
 
 back.addEventListener('click',()=>{
+    audio.cloneNode().play();
     screen.value = screen.value.toString().slice(0,-1);
+    playAudio();
 
 });
 
